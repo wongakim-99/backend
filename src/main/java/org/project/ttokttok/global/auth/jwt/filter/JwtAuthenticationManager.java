168bfc1f1c5cfeae35ced0098b3907316b5e1c12
codemaker-kim <tnals72441@daum.net>
2025-07-01 01:59:09 +0900

    [#8] - jwt 인증 관련 사용자 권한 예외 분기 로직 주석 해제

--- src/main/java/org/project/ttokttok/global/auth/jwt/filter/JwtAuthenticationManager.java
+++ src/main/java/org/project/ttokttok/global/auth/jwt/filter/JwtAuthenticationManager.java
@@ -3,7 +3,9 @@ package org.project.ttokttok.global.auth.jwt.filter;
 import lombok.RequiredArgsConstructor;
 import org.project.ttokttok.domain.admin.exception.AdminNotFoundException;
 import org.project.ttokttok.domain.admin.repository.AdminRepository;
+import org.project.ttokttok.domain.user.repository.UserRepository;
 import org.project.ttokttok.global.auth.jwt.exception.InvalidRoleException;
+import org.project.ttokttok.global.entity.Role;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -18,18 +20,17 @@ public class JwtAuthenticationManager {
 
     // jwt의 인증 권한을 반환해주는 클래스
 
-    //private final UserRepository userRepository;
+    private final UserRepository userRepository;
     private final AdminRepository adminRepository;
 
     private final String ROLE_USER = "ROLE_USER";
     private final String ROLE_ADMIN = "ROLE_ADMIN";
-
-    //todo: merge 후 주석 해제 및 상수 수정
+    
     public Authentication getAuthentication(String email, String role) {
         Object principal = switch (role) {
-            /*case "ROLE_USER" -> userRepository.findByEmail(email)
-                    .orElseThrow(() -> new RuntimeException("User not found"));*/
-            case ROLE_ADMIN-> adminRepository.findByUsername(email)
+            case ROLE_USER -> userRepository.findByEmail(email)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+            case ROLE_ADMIN -> adminRepository.findByUsername(email)
                     .orElseThrow(AdminNotFoundException::new);
             default -> throw new InvalidRoleException();
         };
