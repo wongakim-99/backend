5ab6ea347218c1a4f8d979543b3d47e362e9dc22
codemaker-kim <tnals72441@daum.net>
2025-07-03 03:09:41 +0900

    [#8] - api 양식에 맞게 엔드포인트 네이밍 수정

---
+++ src/main/java/org/project/ttokttok/domain/club/controller/ClubApiController.java
@@ -0,0 +1,12 @@
+package org.project.ttokttok.domain.club.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/clubs")
+public class ClubApiController {
+
+}
