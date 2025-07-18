cd73eca909faf34186a3975b2d114d798aad5e22
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-07-19 01:19:25 +0900

    [#49] - featuer/#49 브랜치랑 병합전 develop 브랜치 세팅

--- src/main/java/org/project/ttokttok/domain/clubboard/service/ClubBoardUserService.java
+++ src/main/java/org/project/ttokttok/domain/clubboard/service/ClubBoardUserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ClubBoardService {
+public class ClubBoardUserService {
 
     private final ClubBoardRepository clubBoardRepository;
 
