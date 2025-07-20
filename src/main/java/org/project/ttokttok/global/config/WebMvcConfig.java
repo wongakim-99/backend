399018d691e2ea57bbefac3d78eaeea016ddd964
codemaker-kim <tnals72441@daum.net>
2025-07-20 20:57:39 +0900

    [#62] - MVC 설정 수정

--- src/main/java/org/project/ttokttok/global/config/WebMvcConfig.java
+++ src/main/java/org/project/ttokttok/global/config/WebMvcConfig.java
@@ -23,13 +23,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolvers.add(authUserInfoResolver);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true) // 자격 증명(쿠키, 인증 헤더 등) 허용
-                .maxAge(3600); // pre-flight 요청 캐싱 시간
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
+//                .allowedHeaders("*") // 모든 헤더 허용
+//                .allowCredentials(true) // 자격 증명(쿠키, 인증 헤더 등) 허용
+//                .maxAge(3600); // pre-flight 요청 캐싱 시간
+//    }
 }
