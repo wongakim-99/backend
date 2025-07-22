b9d3829d164414fb6c2d71a9d964fa9799d7621c
codemaker-kim <tnals72441@daum.net>
2025-07-22 16:13:39 +0900

    [#71] - 지원 폼 조회용 클래스 추가

---
+++ src/main/java/org/project/ttokttok/domain/applyform/controller/ApplyFormUserController.java
@@ -0,0 +1,13 @@
+package org.project.ttokttok.domain.applyform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/forms") // 임시, 추후 변경 가능성 있음.
+public class ApplyFormUserController {
+
+    // TODO: 사용자 지원 폼 조회 API, 사용자 지원 폼 제출 API 구현
+
+
+}
