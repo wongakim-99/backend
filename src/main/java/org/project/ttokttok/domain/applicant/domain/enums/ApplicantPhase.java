cbcca3747c1fa1a9196de68e1ceccc08fbe53b8b
codemaker-kim <tnals72441@daum.net>
2025-08-01 02:29:05 +0900

    [#90] - 현재 지원자 상태 나타내는 열거형 추가

--- src/main/java/org/project/ttokttok/domain/applicant/domain/enums/ApplicantPhase.java
+++ src/main/java/org/project/ttokttok/domain/applicant/domain/enums/ApplicantPhase.java
@@ -1,10 +1,12 @@
 package org.project.ttokttok.domain.applicant.domain.enums;
 
 public enum ApplicantPhase {
-    DOCUMENT_SUBMITTED,
-    DOCUMENT_PASSED,
-    INTERVIEW_SCHEDULED,
-    INTERVIEW_COMPLETED,
-    FINAL_PASSED,
-    FINAL_FAILED
+    DOCUMENT_EVALUATING, // 서류 평가 중
+    DOCUMENT_FAIL, // 서류 불합격
+    DOCUMENT_PASS, // 서류 합격
+    INTERVIEW_EVALUATING, // 면접 평가중
+    INTERVIEW_FAIL, // 면접 불합격
+    INTERVIEW_PASS, // 면접 합격
+    FINAL_PASSED, // 최종 합격
+    FINAL_FAILED // 최종 불합격
 }
